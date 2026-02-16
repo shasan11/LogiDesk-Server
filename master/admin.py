9ef64682b3a8c1320b556343b5d922e9af03f125
Shasan Dhakal <63613238+shasan11@users.noreply.github.com>
2026-02-16 16:47:15 +0545

    Add admin search fields for autocomplete models

--- master/admin.py
+++ master/admin.py
@@ -2,6 +2,13 @@ from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 
+from master.models import Currency
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    search_fields = ("name", "code", "symbol")
+
 # Register your models here.
 
 # Register any remaining models from the master app that do not have a custom admin.
@@ -10,4 +17,3 @@ for model in apps.get_app_config("master").get_models():
         admin.site.register(model)
     except AlreadyRegistered:
         pass
-
